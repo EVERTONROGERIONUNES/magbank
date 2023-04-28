6b24a510e3e4cb88b9163976e096c20e6747e8e9
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-04-27 22:10:27 -0300

    create Account and Institutional page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import Hero from './components/Hero';
 import CenteredButton from './components/CenteredButton';
 import CardList from './components/CardList';
 import CreditCard from './components/CreditCard';
+import AccountModal from './components/AccountModal';
+import Institutional from './components/Institutional';
 
 import posts from './data/posts';
+import { useState } from 'react';
 
+ 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="App">
-      <Navigation />
+      <Navigation handleCreateAccount={() => setShowModal(true)} />
       <Hero />
       <CreditCard />
       <CardList posts={posts} />
       <CenteredButton> 
         Abra a sua conta 
       </CenteredButton>
+      <AccountModal show={showModal} handleClose={() => setShowModal(false)}/>
+      <Institutional />
     </div>
   );
 }
