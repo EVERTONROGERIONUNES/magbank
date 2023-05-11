0128cce2193213f9aaa5e507c471e81bc97e09b3
EVERTONROGERIONUNES <70280660+EVERTONROGERIONUNES@users.noreply.github.com>
2023-05-10 21:48:49 -0300

    finalized project

--- src/components/CenteredButton.js
+++ src/components/CenteredButton.js
@@ -2,12 +2,14 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 
-const CenteredButton = ({ children }) => (
-  <Container className="d-flex justify-content-center"> 
+const CenteredButton = ({ children, onClick }) => (
+  <Container>
     <Row>
-      <Button className="my-5 px-5 py-3" variant="success" size="lg">
-        { children }
-      </Button>
+      <div className="d-flex justify-content-center">
+        <Button className="my-5 px-5 py-3" variant="success" size="lg" onClick={onClick}>
+          {children}
+        </Button>
+      </div>
     </Row>
   </Container>
 )
